5a07aabcc3caea1429359b1b9eadfb9d4fdd4e97
Ross Hill <rosshill@protonmail.com>
2018-03-05 20:46:56 -0500

    use my own words list, cleanup, readme work

--- README.md
+++ README.md
@@ -6,8 +6,15 @@
 ### Features
 * The input field now turns green if input is a valid guess, orange/red if not
 * Input field has a characters remaining indicator
-* Once clue has two or more letters visible, this script will show a list of English words that match the pattern
+* Once the clue has two or more letters visible, this script will show a list of English words that match the pattern
     * This does not work for multi-word phrases
+
+### Words list
+* Skribbler's words list is built from combining the following:
+    * [Google's most commonly used English words](https://github.com/first20hours/google-10000-english) (no swears) – 9899 words
+    * [Dolph's aggregated words list](https://github.com/dolph/dictionary) – 25,322
+    * Combined, these lists come out to the 27,702 unique words in words.txt
+* If you have any good words lists I should add, please let me know!
 
 ### Feedback
 * If you encounter any problems or have a feature request, please [submit an issue](https://github.com/rosslh/skribbler/issues/new)!
--- skribbler.user.js
+++ skribbler.user.js
@@ -3,7 +3,7 @@
 // @namespace https://rosshill.ca
 // @match *://skribbl.io/*
 // @grant none
-// @version 1.0.6
+// @version 1.0.7
 // @author Ross Hill
 // @icon https://skribbl.io/res/favicon.png
 // @homepage https://github.com/rosslh/skribbler
@@ -17,111 +17,99 @@ var prevClue = ""
 
 
 $(document).ready(function() {
-  run = window.setInterval(function() {
-    if($("#currentWord")[0].innerText){
-      clearInterval(run);
-      fetchWordsLists();
-      main();
-    }
-  }, 500);
+    run = window.setInterval(function() {
+        if ($("#currentWord")[0].innerText) {
+            clearInterval(run);
+            fetchWordsLists();
+            main();
+        }
+    }, 500);
 });
 
-function fetchWordsLists(){
-  $.get("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt", function( one ) {
-    words = one.split("\n");
-    $.get("https://raw.githubusercontent.com/dolph/dictionary/master/popular.txt", function( two ) {
-      two.split("\n").forEach(function(item) {
-        if(words.indexOf(item) < 0) {
-          words.push(item);
-        }
-      });
-      words.sort();
+function fetchWordsLists() {
+    $.get("https://raw.githubusercontent.com/rosslh/skribbler/master/words.txt", function(source) {
+        words = source.split("\n");
     });
-  });
 }
 
-function main(){
-  content = document.createElement("span");
-  wordsList = $(document.createElement("ul"));
-  formArea = $("#formChat")[0];
-  content.style.position = "relative";
-  content.style.left = "295px";
-  content.style.top = "-25px";
-  wordsList[0].style.width = "70%";
-  wordsList[0].style.margin = "0 auto";
-  wordsList[0].style.marginTop = "10px";
-  wordsList[0].style.backgroundColor = "#eee";
-  wordsList[0].style.padding = "4px";
-  wordsList[0].style.borderRadius = "2px";
-  wordsList[0].style.listStylePosition = "inside";
-  wordsList[0].style.columns = "4";
-  formArea.appendChild(content);
-  $("#screenGame")[0].appendChild(wordsList[0]);
-  input = $('#inputChat')[0];
-  input.style.border = "3px solid orange";
-
-  window.setInterval(getWords, 500);
-  validate();
-  input.onkeyup = validate;
-}
-
-function validate(){
-  word = $("#currentWord")[0].textContent.toLowerCase();
-  var remaining = word.length - input.value.length;
-  content.textContent = remaining;
-  content.style.color = "unset";
-  if(remaining > 0){
-    content.textContent = "+" + content.textContent;
-    content.style.color = "green";
-  }
-  else if(remaining < 0){
-    content.style.color = "red";
-  }
-  pattern = new RegExp("^" + word.replace(/_/g, "[^- ]") + "$");
-  short = new RegExp("^" + word.substring(0, input.value.length).replace(/_/g, "[^- ]") + "$");
-  if(pattern.test(input.value)){
-    input.style.border = "3px solid green";
-  }
-  else if(short.test(input.value)){
+function main() {
+    content = document.createElement("span");
+    wordsList = $(document.createElement("ul"));
+    formArea = $("#formChat")[0];
+    content.style.position = "relative";
+    content.style.left = "295px";
+    content.style.top = "-25px";
+    wordsList[0].style.width = "70%";
+    wordsList[0].style.margin = "0 auto";
+    wordsList[0].style.marginTop = "10px";
+    wordsList[0].style.backgroundColor = "#eee";
+    wordsList[0].style.padding = "4px";
+    wordsList[0].style.borderRadius = "2px";
+    wordsList[0].style.listStylePosition = "inside";
+    wordsList[0].style.columns = "4";
+    formArea.appendChild(content);
+    $("#screenGame")[0].appendChild(wordsList[0]);
+    input = $('#inputChat')[0];
     input.style.border = "3px solid orange";
-  }
-  else{
-    input.style.border = "3px solid red";
-  }
+
+    window.setInterval(getWords, 500);
+    validate();
+    input.onkeyup = validate;
 }
 
-function getWords(){
-  var clue = $("#currentWord")[0].textContent.toLowerCase();
-  pattern = new RegExp("^" + clue.replace(/_/g, "[^- ]") + "$");
-  if(clue != prevClue){
-    prevClue = clue;
-    while (wordsList[0].firstChild) {
-        wordsList[0].removeChild(wordsList[0].firstChild);
+function validate() {
+    word = $("#currentWord")[0].textContent.toLowerCase();
+    var remaining = word.length - input.value.length;
+    content.textContent = remaining;
+    content.style.color = "unset";
+    if (remaining > 0) {
+        content.textContent = "+" + content.textContent;
+        content.style.color = "green";
+    } else if (remaining < 0) {
+        content.style.color = "red";
     }
-    if(clue.replace(/_/g, "").length > 1){
-      for(var i = 0; i<words.length; i++){
-        if(words[i].length == clue.length && pattern.test(words[i])){
-          var item = document.createElement("li");
-          item.textContent = words[i];
-          wordsList[0].appendChild(item);
-        }
-      }
-      if(wordsList.children().length > 0){
-        var heading = document.createElement("li");
-        heading.textContent = clue +":";
-        wordsList[0].insertBefore(heading, wordsList[0].firstChild)
-      }
+    pattern = new RegExp("^" + word.replace(/_/g, "[^- ]") + "$");
+    short = new RegExp("^" + word.substring(0, input.value.length).replace(/_/g, "[^- ]") + "$");
+    if (pattern.test(input.value)) {
+        input.style.border = "3px solid green";
+    } else if (short.test(input.value)) {
+        input.style.border = "3px solid orange";
+    } else {
+        input.style.border = "3px solid red";
     }
-  }
+}
 
-  if($(wordsList).is(':visible')){
-      if(wordsList.children().length < 2){
-        wordsList.hide();
-      }
-  }
-  else if (wordsList.is(':hidden')){
-      if(wordsList.children().length > 1){
-        wordsList.show();
-      }
-  }
+function getWords() {
+    var clue = $("#currentWord")[0].textContent.toLowerCase();
+    pattern = new RegExp("^" + clue.replace(/_/g, "[^- ]") + "$");
+    if (clue != prevClue) {
+        prevClue = clue;
+        while (wordsList[0].firstChild) {
+            wordsList[0].removeChild(wordsList[0].firstChild);
+        }
+        if (clue.replace(/_/g, "").length > 1) {
+            for (var i = 0; i < words.length; i++) {
+                if (words[i].length == clue.length && pattern.test(words[i])) {
+                    var item = document.createElement("li");
+                    item.textContent = words[i];
+                    wordsList[0].appendChild(item);
+                }
+            }
+            if (wordsList.children().length > 0) {
+                var heading = document.createElement("li");
+                heading.textContent = clue + ":";
+                wordsList[0].insertBefore(heading, wordsList[0].firstChild)
+            }
+        }
+    }
+
+    if ($(wordsList).is(':visible')) {
+        if (wordsList.children().length < 2) {
+            wordsList.hide();
+        }
+    } else if (wordsList.is(':hidden')) {
+        if (wordsList.children().length > 1) {
+            wordsList.show();
+        }
+    }
 }
