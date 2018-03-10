bf2d7a2094b48bd37a3f9afa644f3a7a25ba41e1
Ross Hill <rosshill@protonmail.com>
2018-03-10 11:51:36 -0500

    add downloadurl, remove temp.js

--- skribbler.user.js
+++ skribbler.user.js
@@ -2,8 +2,9 @@
 // @name Skribbler
 // @namespace https://rosshill.ca
 // @match *://skribbl.io/*
-// @version 2.0.1
+// @version 2.0.2
 // @author Ross Hill
+// @downloadURL https://raw.githubusercontent.com/rosslh/skribbler/master/skribbler.user.js
 // @icon https://skribbl.io/res/favicon.png
 // @homepageURL https://github.com/rosslh/skribbler
 // @connect skribbler.herokuapp.com
