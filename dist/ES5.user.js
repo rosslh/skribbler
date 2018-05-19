019f610bfb16608fa72c56ea012c7a2bea449499
Ross Hill <ross@rosshill.ca>
2018-05-18 22:43:45 -0400

    add support metadata

--- dist/ES5.user.js
+++ dist/ES5.user.js
@@ -9,6 +9,7 @@
 // @homepageURL https://github.com/rosslh/skribbler
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @licence MIT
+// @support https://github.com/rosslh/skribbler/issues
 // @grant GM.xmlHttpRequest
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
--- src/metadata.ts
+++ src/metadata.ts
@@ -2,13 +2,14 @@
 // @name Skribbler
 // @namespace https://rosshill.ca
 // @match *://skribbl.io/*
-// @version 2.5.4
+// @version 2.5.5
 // @author Ross Hill <rosshill.ca>
 // @icon https://skribbl.io/res/favicon.png
 // @connect skribbler.herokuapp.com
 // @homepageURL https://github.com/rosslh/skribbler
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @licence MIT
+// @support https://github.com/rosslh/skribbler/issues
 // @grant GM.xmlHttpRequest
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
