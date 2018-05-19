3c08687005fdb3ba7be2c9d00a6481d052c067df
Ross Hill <ross@rosshill.ca>
2018-05-18 22:56:02 -0400

    add support metadata

--- dist/ES5.user.js
+++ dist/ES5.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
+// @author Ross Hill <rosshill.ca>
+// @connect skribbler.herokuapp.com
+// @grant GM_xmlhttpRequest
+// @grant GM.xmlHttpRequest
+// @homepageURL https://github.com/rosslh/skribbler
+// @icon https://skribbl.io/res/favicon.png
+// @licence MIT
+// @match *://skribbl.io/*
 // @name Skribbler
 // @namespace https://rosshill.ca
-// @match *://skribbl.io/*
-// @version 2.5.3
-// @author Ross Hill <rosshill.ca>
-// @icon https://skribbl.io/res/favicon.png
-// @connect skribbler.herokuapp.com
-// @homepageURL https://github.com/rosslh/skribbler
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @licence MIT
 // @support https://github.com/rosslh/skribbler/issues
-// @grant GM.xmlHttpRequest
-// @grant GM_xmlhttpRequest
+// @version 2.5.5
 // ==/UserScript==
 
 // This code has been compiled and minified. The source code and build script can be found on Github.
